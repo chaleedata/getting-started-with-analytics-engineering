1c6382a45242f71a680db003aa766b189e609699
chalee cheappimolchai <57084970+chaleedata@users.noreply.github.com>
2023-01-31 10:04:30 +0000

    update

--- greenery/models/mart/User_count.sql
+++ greenery/models/mart/User_count.sql
@@ -1,3 +1,3 @@
 --select count(distinct user_id) from public.users
 select count(distinct user_id) as user_value
- from {{ source('greenery' , 'users') }}
+ from {{ ref('stg_greenery_user') }}
--- greenery/models/mart/order_count_in_each_order.sql
+++ greenery/models/mart/order_count_in_each_order.sql
@@ -1,7 +1,7 @@
 with order_by_counts_in_each_hours  as (
   select date_trunc('hour' , created_at)
    , count(order_id) as order_count
-  from {{ source('greenery' , 'orders') }}
+  from {{ ref('stg_greenery_orders') }}
   group by 1
 )
 
